c444521a392c072fb12545dedc83e13724226049
Lok Lok <yellowhappy831@gmail.com>
2026-08-15 22:39:28 +0800

    save

--- src/mainview/components/EditorTabs/CharacterSheet.tsx
+++ src/mainview/components/EditorTabs/CharacterSheet.tsx
@@ -25,6 +25,7 @@ const TARGET = 4096;
 function loadImage(src: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image();
+    img.crossOrigin = "anonymous";
     img.onload = () => resolve(img);
     img.onerror = () => reject(new Error("Failed to load image"));
     img.src = src;
